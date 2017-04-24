2f9d26607f3de5456c972c443afb1da89db36779
Zachary Miller <2.4.8.16.32.sixtyfour@gmail.com>
2017-04-23 20:16:13 -0600

    Fixed another type error in the valve driver.

--- valve_driver.py
+++ valve_driver.py
@@ -5,8 +5,9 @@ import json
 import signal
 from threading import Timer
 from datetime import datetime
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import logger
+import traceback
 
 VALVE_DRIVER = "valve_driver.py"
 pin = 7
@@ -57,10 +58,10 @@ def parse_message(message):
 
 
 def rmq_listener(ch, method, properties, body):
-    logger.log("rmq_listener received: " + body)
+    logger.log(VALVE_DRIVER, "rmq_listener received: " + body)
     message = parse_message(body)
     if message is not False and message['ts'] > start_time and message['action'] == "stop":
-        GPIO.output(pin, False)
+        # GPIO.output(pin, False)
         pid = get_pid(schedule_id)
         delete_status_file(schedule_id)
         os.kill(pid, signal.SIGTERM)
@@ -74,9 +75,9 @@ if status_file_exists(schedule_id):
 create_status_file(schedule_id)
 
 # Setup GPIO Output
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(pin, GPIO.OUT)
-GPIO.output(7, True)
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setup(pin, GPIO.OUT)
+# GPIO.output(7, True)
 
 while True:
     try:
@@ -100,5 +101,6 @@ while True:
         channel.start_consuming()
 
     except Exception:
+        traceback.print_exc()
         logger.log(VALVE_DRIVER, "Exception raised")
         logger.log(VALVE_DRIVER, sys.exc_info()[0])
